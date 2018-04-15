a1858d0a4b06702c1d9f1eb73173a7ce4833c177
Joachim Tapparel <joachim.tapparel@epfl.ch>
2018-04-15 11:43:35 +0200

    15.04

--- surface_recon/src/surface_recon.cpp
+++ surface_recon/src/surface_recon.cpp
@@ -10,6 +10,12 @@
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/visualization/interactor_style.h>
 
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/surface/gp3.h>
+
 #include <pcl/filters/filter.h>
 #include <pcl/io/boost.h>
 #include <vtkRenderer.h>
@@ -41,6 +47,10 @@
 #include <vtkGaussianSplatter.h>
 #include <vtkSphereSource.h>
 
+#include <pcl/io/vtk_io.h>
+
+
+#include <vtkDelaunay3D.h>
 #include <ctime>
 int start_time,stop_time;
 
@@ -57,7 +67,64 @@ void run()
      boost::this_thread::sleep(boost::posix_time::microseconds(100000));
    }
  }
-void surface_recon(std::string type)
+
+void delauny()
+{
+
+
+}
+
+void fast_tri()
+{
+	  // Normal estimation*
+	  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
+	  pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+	  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+	  tree->setInputCloud (cloud);
+	  n.setInputCloud (cloud);
+	  n.setSearchMethod (tree);
+	  n.setKSearch (20);
+	  n.compute (*normals);
+	  //* normals should not contain the point normals + surface curvatures
+
+	  // Concatenate the XYZ and normal fields*
+	  pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
+	  pcl::concatenateFields (*cloud, *normals, *cloud_with_normals);
+	  //* cloud_with_normals = cloud + normals
+
+	  // Create search tree*
+	  pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointNormal>);
+	  tree2->setInputCloud (cloud_with_normals);
+
+	  // Initialize objects
+	  pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
+	  pcl::PolygonMesh triangles;
+
+	  // Set the maximum distance between connected points (maximum edge length)
+	  gp3.setSearchRadius (0.025);
+
+	  // Set typical values for the parameters
+	  gp3.setMu (3);
+	  gp3.setMaximumNearestNeighbors (100);
+	  gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
+	  gp3.setMinimumAngle(M_PI/18); // 10 degrees
+	  gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
+	  gp3.setNormalConsistency(false);
+
+	  // Get result
+	  gp3.setInputCloud (cloud_with_normals);
+	  gp3.setSearchMethod (tree2);
+	  gp3.reconstruct (triangles);
+
+	  // Additional vertex information
+	  std::vector<int> parts = gp3.getPartIDs();
+	  std::vector<int> states = gp3.getPointStates();
+
+	 viz.addPolygonMesh(triangles);
+
+
+}
+void gauss()
 {
 	  // Create points
 	  vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
@@ -65,48 +132,29 @@ void surface_recon(std::string type)
 	  for(pcl::PointCloud<pcl::PointXYZ>::iterator it = cloud->begin();it != cloud->end(); it++)
 	  {
 	     points->InsertNextPoint(it->x,it->y,it->z);
-
 	  }
-
 	  vtkSmartPointer<vtkPolyData> polydata =  vtkSmartPointer<vtkPolyData>::New();
 	  polydata->SetPoints(points);
 
 	  vtkSmartPointer<vtkContourFilter> surface =   vtkSmartPointer<vtkContourFilter>::New();
 
-	  if(type==std::string("gauss"))
-	  {
-		  vtkSmartPointer<vtkGaussianSplatter> splatter =  vtkSmartPointer<vtkGaussianSplatter>::New();
 
-		  splatter->SetInputData(polydata);
+	  vtkSmartPointer<vtkGaussianSplatter> splatter =  vtkSmartPointer<vtkGaussianSplatter>::New();
 
-		  unsigned int n = 150;
-		  splatter->SetSampleDimensions(n,n,n);
-		  splatter->SetRadius(.02);
-		  splatter->SetExponentFactor(-10);
-		  splatter->SetEccentricity(2);
-		  splatter->Update();
+	  splatter->SetInputData(polydata);
 
-		  surface->SetInputConnection(splatter->GetOutputPort());
-		  surface->SetValue(0,.95* splatter->GetRadius() );
+	  unsigned int n = 150;
+	  splatter->SetSampleDimensions(n,n,n);
+	  splatter->SetRadius(.02);
+	  splatter->SetExponentFactor(-10);
+	  splatter->SetEccentricity(2);
+	  splatter->Update();
 
-	  }
+	  surface->SetInputConnection(splatter->GetOutputPort());
+	  surface->SetValue(0,.95* splatter->GetRadius() );
 
-	  else if(type==std::string("check"))
-	  {
-		  vtkSmartPointer<vtkCheckerboardSplatter> splatter =  vtkSmartPointer<vtkCheckerboardSplatter>::New();
 
-		  splatter->SetInputData(polydata);
 
-		  unsigned int n = 150;
-		  splatter->SetSampleDimensions(n,n,n);
-		  splatter->SetRadius(.02);
-		  splatter->SetExponentFactor(-10);
-		  splatter->SetEccentricity(2);
-		  splatter->Update();
-
-		  surface->SetInputConnection(splatter->GetOutputPort());
-		  surface->SetValue(0, 0.01);
-	  }
 
 	  // Create a mapper and actor
 	  vtkSmartPointer<vtkPolyDataMapper> mapper =   vtkSmartPointer<vtkPolyDataMapper>::New();
@@ -127,7 +175,8 @@ int main(int argc, char ** argv)
 	start_time=clock();
 	if(argc!=3)
 	{
-		printf("./surface_recon <gauss/check> cloudpath \n");
+		printf("./surface_recon <fast/gauss> cloudpath \n");
+		printf("exemple: ./surface_recon fast bunny.pcd \n");
 		return EXIT_FAILURE;
 	}
 
@@ -139,12 +188,13 @@ int main(int argc, char ** argv)
 
 	viz.addPointCloud (cloud, "original_cloud");
 
-	surface_recon(recon_type);
+	 if(recon_type==std::string("delauny"))delauny();
+	 if(recon_type==std::string("fast"))fast_tri();
+	 if(recon_type==std::string("gauss"))gauss();
 
 	stop_time=clock();
 	cout << "\nExec time: " << (stop_time-start_time)/double(CLOCKS_PER_SEC)*1000<< " ms "<< endl;
 	run();
-
 
 	return EXIT_SUCCESS;
 
